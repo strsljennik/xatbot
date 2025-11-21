001bd3466bec73570a0c96266c712cca83acc960
strsljennik <strsljennik@gmail.com>
2025-11-21 10:51:42 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import { config } from 'dotenv';
 import { sequelize } from './core/Database.js';
 import { Bot } from './core/Bot.js';
-import './ping.js'
+import './ping.js';
+import express from 'express';
 
 config();
+
+// Start small HTTP server for Render health check
+const app = express();
+const PORT = process.env.PORT || 10000;
+
+app.get('/', (req, res) => res.send('Bot is running'));
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 (async () => {
     await sequelize.authenticate();
